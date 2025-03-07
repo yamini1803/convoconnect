1fea4e1185a15a0fe07224df6153d13ca4b239dd
yamini1803 <yaminivelithota@gmail.com>
2025-03-07 15:58:35 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
+import roomHandler from "./socket/roomHandler.js";
+
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/auth", authRoutes);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  roomHandler(socket);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+const PORT = 6001;
+mongoose
+  .connect("mongodb://127.0.0.1:27017/meet-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+server.listen(PORT, () => {
+  console.log(`Running @ ${PORT}`);
+});
